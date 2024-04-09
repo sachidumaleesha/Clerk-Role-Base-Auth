cd182ddb1845d4f14a2601088f52c81eaceb998f
sachidumaleesha <diwansachidu2000@gmail.com>
2024-04-09 14:22:32 +0530

    completed

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+export default function AuthLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+        <div className="flex justify-center items-center h-screen">
+          {children}
+        </div>
+    );
+  }
