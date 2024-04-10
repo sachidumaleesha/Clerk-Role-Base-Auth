47181b7d2fde820e51f3dfd5f85c7dcac6e8980e
sachidumaleesha <diwansachidu2000@gmail.com>
2024-04-10 17:43:47 +0530

    add webhook

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
+import {
+  OrganizationList,
+  OrganizationProfile,
+  OrganizationSwitcher,
+  SignOutButton,
+  UserButton,
+} from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       HI There
+      <UserButton />
+      <OrganizationProfile />
+      <OrganizationSwitcher />
     </div>
   );
 }
